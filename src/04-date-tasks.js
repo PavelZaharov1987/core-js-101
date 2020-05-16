d0c7121d6916e9cfdcb93d41fd4a83df4dae7fb2
PavelZakharov <pavelzakharov1987@gmail.com>
2020-05-16 14:10:18 +0300

    feat: add date script

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 
@@ -73,8 +74,24 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let timeSpan = endDate.getTime() - startDate.getTime();
+  const sss = timeSpan % 1000;
+  timeSpan = (timeSpan - sss) / 1000;
+  const ss = timeSpan % 60;
+  timeSpan = (timeSpan - ss) / 60;
+  const mm = timeSpan % 60;
+  const HH = (timeSpan - mm) / 60;
+  let temp = `${(HH > 12) ? HH - 12 : `0${HH}`}`;
+  if (HH === 0) temp = '00';
+  temp += ((mm < 10) ? ':0' : ':') + mm;
+  temp += ((ss < 10) ? ':0' : ':') + ss;
+  temp += ((sss === 0) ? '.00' : '.') + sss;
+  return temp;
+  // return `${HH.toString().padStart(2, '0')}:
+  // ${mm.toString().padStart(2, '0')}:
+  // ${ss.toString().padStart(2, '0')}.
+  // ${sss.toString().padStart(3, '0')}`;
 }
 
 
@@ -94,8 +111,20 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const timeSpan = new Date(date);
+  const hours = timeSpan.getUTCHours();
+  const minutes = timeSpan.getUTCMinutes();
+
+  const hoursAngle = 0.5 * (60 * hours + minutes);
+  const minutesAngle = 6 * minutes;
+
+  let angle = Math.abs(hoursAngle - minutesAngle);
+  while (angle > 180) {
+    angle = Math.abs(360 - angle);
+  }
+
+  return (angle * Math.PI) / 180;
 }
 
 
